2981930b15beb4ebd8c36eaba175f2abaab904cf
wf <willfreyman@gmail.com>
2014-01-17 14:33:08 -0800

    update

--- views/publications.php
+++ views/publications.php
@@ -7,14 +7,16 @@
       		<div class="row-fluid nice_margins">
       			<div class="span12">
 					<h4>Abstracts</h4>
+					<p>Schneider, A.C., Guilliams, C.M., Freyman, W.A., Springer, Y., and B.G. Baldwin. 2014. Species relationships in <em>Hesperolinon</em> (Linaceae) inferred from ITS and chloroplast sequence data. Northern California Botanists Symposium, Chico, CA</p>
+					
 					<p>Freyman, W.A. and J.P. Olfelt. 2013. Phylogeny and divergence times among the North American <em>Rhodiola</em> (Crassulaceae) species. Botany 2013, New Orleans, LA</p>
 					<p>Freyman, W.A. and K.A. Glennemeier. 2013. Restoration Map: a tool for stewards to plan and assess restoration projects. Wild Things Conference, Chicago, IL</p>
 					<p>Freyman, W.A., M.J. Rios, and J.P. Olfelt. 2012. Molecular phylogeny of the North American <em>Rhodiola</em> (Crassulaceae). SACNAS National Conference, Seattle, WA</p>
 					<p>Freyman, W.A., S. Packard, and J. Pepper. 2012. Restoration Map: a web-based tool for spatially explicit planning of ecological restoration projects. Ecological Society of America Annual Meeting, Portland, OR</p>
 					<p>Freyman, W.A., M.J. Rios, and J.P. Olfelt. 2012. Relationships of North American <em>Rhodiola</em> species using DNA sequence data. NEIU Student Research Symposium, Chicago, IL</p>
 					<h4>Articles</h4>
-					<p>Olfelt, J.P. and W.A. Freyman. In prep. Relationships of the North American <em>Rhodiola</em> (Crassulaceae).</p>
-					<p>Freyman, W.A. and K.A. Glennemeier. Accepted. Restoration Map: a web-based tool for spatial and participatory adaptive management of ecological restoration projects. Ecological Restoration.</p>
+					<p>Olfelt, J.P. and W.A. Freyman. Submitted. Relationships of the North American <em>Rhodiola</em> (Crassulaceae).</p>
+					<p>Freyman, W.A. and K.A. Glennemeier. 2014. Restoration Map: a web-based tool for spatial and participatory adaptive management of ecological restoration projects. <em>Ecological Restoration</em> (32-1)</p>
 				</div>
       		</div>
       		<hr>
